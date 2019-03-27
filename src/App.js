874548ec21a42261f7905efd00aa280130e8fb82
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-03-27 15:33:34 -0700

    Set up basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Wrapper from './components/Wrapper';
 import './App.css';
 
 class App extends Component {
+  state = {
+    score,
+    clicked
+  };
+
+// Listen for clicks
+  clickTile = () => {
+    // Check to see if image has been clicked already
+
+    // If clicked = false, +1 to score
+
+    // Change clicked: false > clicked: true
+  }
+
+  // Map over tiles to render each image
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <NavBar>Clicky Game</NavBar>
+        <Header> </Header>
+        <Tiles />
+        <Footer />
+      </Wrapper>
     );
   }
 }
