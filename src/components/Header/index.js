bb8652e030fb350b619452b38b8b23b5bc892590
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-03-27 17:03:23 -0700

    Updated Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function Header() {
     return <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1 class="display-4">Fluid jumbotron</h1>
-            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <h1 class="display-4">The Clicky Game</h1>
+            <p class="lead">Click on an image to earn points - but don't click on it more than once or you lose!</p>
         </div>
     </div>
 }
