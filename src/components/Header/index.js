7a847966e4cea57985bbb938cbe1382a8931aa41
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-03 11:12:37 -0700

    Changed class to className

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./style.css";
 
 function Header() {
-    return <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">The Clicky Game</h1>
-            <p class="lead">Click on an image to earn points - but don't click on it more than once or you lose!</p>
+    return <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">The Clicky Game</h1>
+            <p className="lead">Click on an image to earn points - but don't click on it more than once or you lose!</p>
         </div>
     </div>
 }
