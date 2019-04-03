dc003e2af8ea8fac2b3c9adb96ecb56458b7fd6f
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-03 11:13:25 -0700

    Changed class to className

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,21 +2,21 @@ import React from "react";
 import "./style.css";
 
 function NavBar() {
-    return <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    return <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">ALERT</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="#">ALERT</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Score: </a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Score: </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">High Score: </a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">High Score: </a>
                 </li>
             </ul>
         </div>
