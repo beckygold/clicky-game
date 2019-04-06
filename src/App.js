4951286069c2deda6718a5990831f44bead4454d
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-06 10:53:36 -0700

    Tweaked formatting

--- src/App.js
+++ src/App.js
@@ -15,50 +15,52 @@ class App extends Component {
   };
 
 
-// Listen for image clicks
+  // Listen for image clicks
   clickImage = event => {
     event.preventDefault()
 
     console.log(event.target.src)
     // Check to see if image has been clicked already
 
-      // If clicked = false, +1 to score
+    // If clicked = false, +1 to score
 
-        // Change 'clicked: false' -> 'clicked: true'
+    // Change 'clicked: false' -> 'clicked: true'
 
-        // Alert user of correct guess
+    // Alert user of correct guess
 
-        // Shuffle images
-    
+    // Shuffle images
+
     // If clicked = true, set score to 0
 
-      // Reset all images clicked value to false
+    // Reset all images clicked value to false
 
-      // Alert user of incorrect guess
+    // Alert user of incorrect guess
 
-      // Shuffle images
+    // Shuffle images
   }
 
   render() {
     return (
-      <Wrapper>
-        {/* <NavBar> </NavBar> */}
-        {/* <Header> </Header> */}
-        {/* Map over images array to render image thumbnails */}
-        {this.state.images.map(image => (
-          
-          <Image 
-            clickFunction={this.clickImage}
-            key={image.id}
-            id={image.id}
-            name={image.name}
-            src={process.env.PUBLIC_URL + image.src}
-          // process.env.PUBLIC_URL will look in the public folder for the images
-          // image={process.env.PUBLIC_URL + (passed parameter from map).image(?)}
-          />
+      <div>
+        <NavBar> </NavBar>
+        <Header> </Header>
+        <Wrapper>
+          {/* Map over images array to render image thumbnails */}
+          {this.state.images.map(image => (
+
+            <Image
+              clickFunction={this.clickImage}
+              key={image.id}
+              id={image.id}
+              name={image.name}
+              src={process.env.PUBLIC_URL + image.src}
+            // process.env.PUBLIC_URL will look in the public folder for the images
+            // image={process.env.PUBLIC_URL + (passed parameter from map).image(?)}
+            />
           ))}
+        </Wrapper>
         <Footer />
-      </Wrapper>
+      </div>
     );
   }
 }
