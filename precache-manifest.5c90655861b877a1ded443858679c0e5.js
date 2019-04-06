883bd1c117258080464aeaeca5db6fa4a84999f8
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-04-06 12:06:20 -0700

    Updates

---
+++ precache-manifest.5c90655861b877a1ded443858679c0e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "abf275aafa3abe03dd6f",
+    "url": "/clicky-game/static/js/main.e36b36db.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/clicky-game/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "abf275aafa3abe03dd6f",
+    "url": "/clicky-game/static/css/main.e0aa625b.chunk.css"
+  },
+  {
+    "revision": "0fb6a53693938618d17af691856733e1",
+    "url": "/clicky-game/index.html"
+  }
+];
